1f78bd383410cb00ed45c4fa1fa6ec9306656071
satej <satejrane04@gmail.com>
2020-11-21 16:36:13 -0600

    second commit

---
+++ bootstrap.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Update hosts file
+echo "[TASK 1] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+127.0.0.1 `hostname` `hostname`
+127.0.0.1 localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1       localhost localhost.localdomain localhost6 localhost6.localdomain6
+192.168.1.150 k8s-master k8s-master
+192.168.1.151 k8s-worker1 k8s-worker1
+192.168.1.152 k8s-worker2 k8s-worker2
+EOF
+
+# Install docker from Docker-ce repository
+echo "[TASK 2] Install docker container engine"
+yum install -y -q yum-utils device-mapper-persistent-data lvm2 > /dev/null 2>&1
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo > /dev/null 2>&1
+yum install -y -q docker-ce >/dev/null 2>&1
+
+# Enable docker service
+echo "[TASK 3] Enable and start docker service"
+mkdir /etc/docker
+cat << EOF > /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+mkdir -p /etc/systemd/system/docker.service.d
+systemctl enable docker >/dev/null 2>&1
+systemctl start docker
+
+# Disable SELinux
+echo "[TASK 4] Disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+# Stop and disable firewalld
+echo "[TASK 5] Stop and Disable firewalld"
+systemctl disable firewalld >/dev/null 2>&1
+systemctl stop firewalld
+
+# Add sysctl settings
+echo "[TASK 6] Add sysctl settings"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system >/dev/null 2>&1
